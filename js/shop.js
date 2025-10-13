3f40032b15de7a3d146b0550ab106e42a9007e2f
Việt Anh <vivetanh@gmail.com>
2025-10-13 19:59:06 +0700

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -24,3 +24,33 @@ function buyItem(id) {
   .then(res => res.json())
   .then(res => alert(res.message));
 }
+
+function buyItem(id) {
+  const username = prompt("Nhập tên người chơi:");
+  fetch('/api/shop', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ itemId: id, username })
+  })
+  .then(res => res.json())
+  .then(async res => {
+    alert(res.message);
+
+    // gửi lệnh sang Discord
+    const commands = ['!add cap 1', '!add cap 2', '!add cap 3'];
+    const msg = commands[id - 1]; // id 1 -> cap 1, id 2 -> cap 2,...
+    
+    const discordRes = await fetch('/api/sendToDiscord', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ content: msg })
+    });
+
+    if (discordRes.ok) {
+      alert(`💬 Đã gửi yêu cầu "${msg}" lên Discord!`);
+    } else {
+      alert('❌ Gửi lệnh Discord thất bại.');
+    }
+  });
+}
+
